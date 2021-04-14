ea67b66190c239e984f024c40ebd779f22e2a767
AnddyAnddy <anddymamouni@gmail.com>
2021-04-14 12:57:50 +0200

    Task-2 / Obj 1 / C  transform, accumulate

--- src/geometry.hpp
+++ src/geometry.hpp
@@ -5,6 +5,7 @@
 #include <cassert>
 #include <cmath>
 #include <iostream>
+#include <numeric>
 
 struct Point2D
 {
@@ -64,7 +65,7 @@ struct Point2D
 
 struct Point3D
 {
-    float values[3] {};
+    std::array<float, 3> values;
 
     Point3D() {}
     Point3D(float x, float y, float z) : values { x, y, z } {}
@@ -80,25 +81,23 @@ struct Point3D
 
     Point3D& operator+=(const Point3D& other)
     {
-        x() += other.x();
-        y() += other.y();
-        z() += other.z();
+        int i = 0;
+        std::transform(values.begin(), values.end(), values.begin(),
+                       [&other, &i](auto v) { return v + other.values[i++]; });
         return *this;
     }
 
     Point3D& operator-=(const Point3D& other)
     {
-        x() -= other.x();
-        y() -= other.y();
-        z() -= other.z();
+        int i = 0;
+        std::transform(values.begin(), values.end(), values.begin(),
+                       [&other, &i](auto v) { return v - other.values[i++]; });
         return *this;
     }
 
     Point3D& operator*=(const float scalar)
     {
-        x() *= scalar;
-        y() *= scalar;
-        z() *= scalar;
+        std::transform(values.begin(), values.end(), values.begin(), [scalar](auto v) { return v * scalar; });
         return *this;
     }
 
@@ -125,7 +124,11 @@ struct Point3D
 
     Point3D operator-() const { return Point3D { -x(), -y(), -z() }; }
 
-    float length() const { return std::sqrt(x() * x() + y() * y() + z() * z()); }
+    float length() const
+    {
+        return std::sqrt(std::accumulate(values.begin(), values.end(), 0.f,
+                                         [](const auto v1, const auto v2) { return v1 + v2 * v2; }));
+    }
 
     float distance_to(const Point3D& other) const { return (*this - other).length(); }
 
