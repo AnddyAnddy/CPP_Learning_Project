dcb2173ee9e677b646828aa3993e600782401330
AnddyAnddy <anddymamouni@gmail.com>
2021-04-15 17:22:51 +0200

    Task-4 / Obj 2 / 1

--- src/geometry.hpp
+++ src/geometry.hpp
@@ -158,6 +158,11 @@ struct Point3D
     }
 };
 
+template <typename T, int Dimension> struct Point
+{
+    std::array<T, Dimension> values;
+    Point() {}
+};
 // our 3D-coordinate system will be tied to the airport: the runway is parallel to the x-axis, the z-axis
 // points towards the sky, and y is perpendicular to both thus,
 // {1,0,0} --> {.5,.5}   {0,1,0} --> {-.5,.5}   {0,0,1} --> {0,1}
